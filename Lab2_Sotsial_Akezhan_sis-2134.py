77ba16dd0143668bef486fbbbe1a71971adbe99e
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2023-09-27 18:09:11 +0600

    created lab 2 and solved first tasks

---
+++ Lab2_Sotsial_Akezhan_sis-2134.py
@@ -0,0 +1,25 @@
+# #task 1
+# try:
+#    num = int(input())
+#    first_num = num // 1000
+#    second_num = (num // 100) % 10
+#    third_num = (num // 10) % 10
+#    last_num = num % 10
+#    if ((first_num + last_num) == (second_num - third_num)):
+#       print('yes')
+#    else:
+#       print('no')
+# except ValueError:
+#    print('error')
+
+#task 2
+# try:
+#    age = int(input())
+#    if (age >= 18 <= 110):
+#       print('access allowed')
+#    else:
+#       print('access denied')
+# except ValueError:
+#    print('error')
+
+#task 3
